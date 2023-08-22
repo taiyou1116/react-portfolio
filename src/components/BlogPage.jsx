6f15e65a696b4d0a2cd0cb42e63c8088389e5786
taiyou <hiromacha1116@icloud.com>
2023-08-22 17:50:55 +0900

    fix my info

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function BlogPage() {
   return (
-    <div>BlogPage</div>
+    <div className='container'>
+      <h1>BLOG</h1>
+
+      <p>
+      </p>
+    </div>
   )
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
+import reactImage from "../Images/react.png";
+import jsImage from "../Images/js.png";
+import firebaseImage from "../Images/firebase.jpg";
+import profileImage from "../Images/google-bear.jpeg";
 
 function HomePage() {
   return (
-    <div>
-      
+    <div className="container text-center">
+      <h1>Taiyou</h1>
+
+      <img src={profileImage} className="profileImage" />
+
+      <p>
+        
+      </p>
+
+      <section class="page-section" id="services">
+        <div className="service">
+          <div class="text-center">
+            <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
+            <h3 class="section-subheading text-muted mb-5">
+              私が作った作品一覧です
+            </h3>
+          </div>
+          <div class="row text-center">
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+                <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 class="my-3">no data</h4>
+              <p class="text-muted">
+                
+              </p>
+            </div>
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+                <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 class="my-3">no data</h4>
+              <p class="text-muted">
+                
+              </p>
+            </div>
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+                <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 class="my-3">no data</h4>
+              <p class="text-muted">
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="skill">
+        <div class="text-center">
+          <h1 class="title">スキル</h1>
+          <div class="row text-center">
+            <div class="col-md-4 services">
+              <img src={reactImage} />
+              <h4>React</h4>
+              <p>React学習中</p>
+            </div>
+            <div class="col-md-4 services">
+              <img src={jsImage} />
+              <h4>HTML/CSS</h4>
+              <p>HTML/CSS学習中</p>
+            </div>
+            <div class="col-md-4 services">
+              <img src={firebaseImage} />
+              <h4>Firebase</h4>
+              <p>Firebase学習中</p>
+            </div>
+          </div>
+          {/* <button type="button" class="btn btn-primary">
+            スキル一覧
+          </button> */}
+        </div>
+      </section>
     </div>
   )
 }
